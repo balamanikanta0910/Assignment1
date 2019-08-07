0429ee924fe7239eb0140980000deeecbb9b16b7
bala manikanta <balamanikanta@balas-MacBook-Pro.local>
2019-08-07 19:01:15 +0530

    Intial commit

--- src/main/java/RnaTranscription.java
+++ src/main/java/RnaTranscription.java
@@ -1,7 +1,27 @@
-class RnaTranscription {
+import java.util.*;
+
+public class RnaTranscription {
+
+    public static void main(String args[])
+    {
+        String s = new RnaTranscription().transcribe("CATG");
+        System.out.print(s);
+    }
 
     String transcribe(String dnaStrand) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+
+        HashMap<Character,Character> map = new HashMap<>();
+        map.put('C','G');
+        map.put('G','C');
+        map.put('T','A');
+        map.put('A','U');
+
+        String rna = new String("");
+
+        for(int i=0; i<dnaStrand.length(); i++)
+        rna += map.get(dnaStrand.charAt(i));
+
+        return rna;
     }
 
 }
